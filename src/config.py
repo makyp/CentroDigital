ac428c7c1d94776bbc1beb650e4d9bd4faa932b7
Makyp_ <maquip02@hotmail.com>
2024-10-21 23:19:16 -0500

    Botones

--- src/config.py
+++ src/config.py
@@ -1,13 +1,8 @@
 from pymongo import MongoClient
 import certifi
-<<<<<<< HEAD
-MONGO='mongodb+srv://makyp:Pacho040321@cluster0.yjkbst6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-#MONGO='mongodb+srv://Aleky:MpBn7j6kTRdLXgDR@centrodigitaldedesarrol.uksf6.mongodb.net/?retryWrites=true&w=majority&appName=CentroDigitaldeDesarrollo'
-=======
 
 MONGO='mongodb+srv://makyp:Pacho040321@cluster0.yjkbst6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
->>>>>>> 741c5c7abad752f16b83847f7d6dff0190bf89c1
 
 certificado = certifi.where()
 
